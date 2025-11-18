102e9fbae835e91b4cea9cfe12f36be8ab2c354d
Salvo Cavallaro <salvator.cavallaro@gmail.com>
2025-11-18 23:54:12 +0100

    added 39 combination sum

---
+++ Medium/backtracking/39_combination_sum/solution.py
@@ -0,0 +1,16 @@
+class Solution:
+    def backtrack(self, candidates, start, candidate, solution, target):
+        if sum(candidate) == target:
+            solution.append(candidate.copy())
+        elif sum(candidate) < target:
+            for i in range(start, len(candidates)):
+                num = candidates[i]
+                candidate.append(num)
+                self.backtrack(candidates, i, candidate, solution, target)
+                candidate.pop()
+
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        solution = []
+        self.backtrack(candidates, 0, [], solution, target)
+        return solution
+        
