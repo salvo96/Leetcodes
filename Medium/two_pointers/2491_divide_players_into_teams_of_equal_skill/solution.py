3c25576cd0bbbef25f61c8a7e77d9b0f111a47d9
Salvo Cavallaro <salvator.cavallaro@gmail.com>
2026-04-11 21:57:54 +0200

    fixed 2491_divide_players_into_teams_of_equal_skill

--- Medium/two_pointers/2491_divide_players_into_teams_of_equal_skill/solution.py
+++ Medium/two_pointers/2491_divide_players_into_teams_of_equal_skill/solution.py
@@ -3,22 +3,20 @@ class Solution:
         num_teams = len(skill) / 2
 
         sum_skills = 0
-        for player_skill in skill:
-            sum_skills += player_skill
+        for num in skill:
+            sum_skills += num
 
-        team_sum = sum_skills // num_teams
-        if sum_skills % num_teams != 0:
-            return -1
+        team_sum = sum_skills / num_teams
 
-        skill_match_dict = dict()
-        for player_skill in skill:
-            skill_match_dict[team_sum - player_skill] = player_skill
+        skill = sorted(skill)
+
+        left, right = 0, len(skill) - 1
 
         sum_chemistry = 0
-        for player_skill in skill:
-            skill_value = skill_match_dict.get(player_skill)
-            if not skill_value:
+        while left < right:
+            if skill[left] + skill[right] != team_sum:
                 return -1
-            sum_chemistry += player_skill * skill_value
+            sum_chemistry += skill[left] * skill[right]
+            left, right = left + 1, right - 1
 
-        return int(sum_chemistry / 2)
+        return sum_chemistry
