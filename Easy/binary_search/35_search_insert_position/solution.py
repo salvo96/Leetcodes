3c96ccbe46b92487dec6803e743af7e2018ccee7
Salvo Cavallaro <salvator.cavallaro@gmail.com>
2026-04-21 08:09:53 +0200

    added 35_search_insert_position

---
+++ Easy/binary_search/35_search_insert_position/solution.py
@@ -0,0 +1,14 @@
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums) - 1
+
+        while left <= right:
+            middle = (left + right) // 2
+            if target < nums[middle]:
+                right = middle - 1
+            elif target > nums[middle]:
+                left = middle + 1
+            else:
+                return middle
+
+        return left
