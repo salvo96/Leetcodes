15d4e2cfdd03ebbbd50762dc0233d00f0a139141
Salvo Cavallaro <salvator.cavallaro@gmail.com>
2026-05-01 20:41:17 +0200

    added 19_remove_nth_node_from_end_of_list

---
+++ Medium/linked_list/19_remove_nth_node_from_end_of_list/solution.py
@@ -0,0 +1,41 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+################################################################## Iterative Solution ##################################################################
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        counter = 0
+        
+        node = head
+        while node:
+            counter += 1
+            node = node.next
+
+        if counter == n:
+            return head.next
+
+        node = head
+        while node:
+            counter -= 1
+            if counter == n:
+                node.next = node.next.next
+                return head
+            node = node.next
+        
+################################################################## Recursive Solution ##################################################################
+class Solution:
+    def __init__(self, ):
+        self.counter = 0
+
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        if head.next:
+            head.next = self.removeNthFromEnd(head.next, n)
+        
+        self.counter += 1
+
+        if self.counter == n:
+            return head.next
+
+        return head
