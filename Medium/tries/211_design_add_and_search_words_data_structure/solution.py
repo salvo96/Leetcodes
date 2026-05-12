2dcbfbf3aab953ce0ea2ff2a04e2675dd30a186c
Salvo Cavallaro <salvator.cavallaro@gmail.com>
2026-05-12 23:56:01 +0200

    added 211_design_add_and_search_words_data_structure

---
+++ Medium/tries/211_design_add_and_search_words_data_structure/solution.py
@@ -0,0 +1,54 @@
+class Node:
+    def __init__(
+        self,
+    ):
+        self.next_letter = 26 * [None]
+        self.is_word = False
+
+
+class WordDictionary:
+    def __init__(self):
+        self.head = Node()
+
+    def addWord(self, word: str) -> None:
+        node = self.head
+        for letter in word:
+            c = ord(letter) - 97
+
+            if not node.next_letter[c]:
+                node.next_letter[c] = Node()
+            node = node.next_letter[c]
+
+        node.is_word = True
+
+        return
+
+    def search(self, word: str) -> bool:
+        def search_substr(node: Node, word: str, start_idx: int = 0):
+            for idx in range(start_idx, len(word)):
+                letter = word[idx]
+
+                if letter != ".":
+                    c = ord(letter) - 97
+
+                    if not node.next_letter[c]:
+                        return False
+
+                    node = node.next_letter[c]
+                else:
+                    res = False
+                    for elem in node.next_letter:
+                        if elem:
+                            res = max(res, search_substr(elem, word, idx + 1))
+
+                    return res
+
+            return node.is_word
+
+        return search_substr(self.head, word)
+
+
+# Your WordDictionary object will be instantiated and called as such:
+# obj = WordDictionary()
+# obj.addWord(word)
+# param_2 = obj.search(word)
